11c7bd89c712062e24f1ecb9f131a674b8a406fd
sindhujapadmabhanu <86618210+sindhujapadmabhanu@users.noreply.github.com>
2023-09-06 10:39:31 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,7 +6,7 @@
 # streamlit.text('🍞yui')
 
 import pandas
-my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlist.dataframe(my_fruit_list)
 
 
