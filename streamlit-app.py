b97b2c967fe44f6ea488022b6423f39532e68863
sindhujapadmabhanu <86618210+sindhujapadmabhanu@users.noreply.github.com>
2023-09-06 10:37:45 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,8 +1,13 @@
-import streamlit
-streamlit.title('Hello world')
-streamlit.header('afghj')
-streamlit.text('🥣vbnm')
-streamlit.text('🥗ghjk')
-streamlit.text('🍞yui')
+ import streamlit
+# streamlit.title('Hello world')
+# streamlit.header('afghj')
+# streamlit.text('🥣vbnm')
+# streamlit.text('🥗ghjk')
+# streamlit.text('🍞yui')
+
+import pandas
+my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.dataframe(my_fruit_list)
+
 
 
