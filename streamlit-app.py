da5cb201b9117f458ebd3a6ea528d854dc187c3c
sindhujapadmabhanu <86618210+sindhujapadmabhanu@users.noreply.github.com>
2023-09-11 11:18:25 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -15,7 +15,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.text(fruityvice_response)
 streamlit.dataframe(fruityvice_response)
 
